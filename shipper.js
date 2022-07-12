1d8094054cbedb22baee5fe3b2b00743f59a5905
dangchien26012003 <108055518+dangchien26012003@users.noreply.github.com>
2022-07-13 01:41:13 +0700

    Add files via upload

---
+++ shipper.js
@@ -0,0 +1,11 @@
+var check = document.getElementById('navbar');
+check.onclick = function() {
+    check.classList.toggle('open')
+    $('.info-shiper').toggleClass('open')
+    $('.info-shiper').hasClass('open').hide();
+}
+
+// donhien ta
+
+
+
